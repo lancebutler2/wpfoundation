cb65b0633cc1dbc7a02ce9f58d7860b66fb9cb90
Lance Butler II <lancebutler2@gmail.com>
2014-03-26 22:45:31 -0400

    updated gulpfile.js to concat/uglify all foundation scripts

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    changed = require('gulp-changed'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
-    gulp.src('scss/app.scss')
+    gulp.src('./scss/app.scss')
         .pipe(sass({loadPath: ['bower_components/foundation/scss'], quiet: true, trace: true, style: 'nested'}))
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
         .pipe(gulp.dest('stylesheets'))
@@ -14,8 +17,18 @@ gulp.task('styles', function() {
         .pipe(gulp.dest('stylesheets'))
 });
 
+gulp.task('foundation', function() {
+    gulp.src(['./bower_components/foundation/js/vendor/fastclick.js', './bower_components/foundation/js/vendor/placeholder.js', './bower_components/foundation/js/vendor/jquery.cookie.js', './bower_components/foundation/js/vendor/modernizr.js', './bower_components/foundation/js/foundation/foundation.js', './bower_components/foundation/js/foundation/foundation.*.js'])
+        .pipe(changed('./js/'))
+        .pipe(concat('foundation.all.js'))
+        .pipe(gulp.dest('./js/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('./js/'))
+});
+
 gulp.task('watch', function() {
-    gulp.watch(['scss/*.scss', 'gulpfile.js'], ['styles']);
+    gulp.watch(['./scss/*.scss', './gulpfile.js'], ['styles']);
 });
 
 gulp.task('default', ['watch']);
